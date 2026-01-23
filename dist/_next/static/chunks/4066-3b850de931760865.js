6f39f51d0956fa3f6ddca0c6e1ce819d88ff1fef
pradeep <pradeepandigital@gmail.com>
2026-01-23 23:10:07 +0000

    fix: Copy build output to dist directory for Cloudflare Pages compatibility

---
+++ dist/_next/static/chunks/4066-3b850de931760865.js
@@ -0,0 +1,81 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4066],{1976:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]])},8281:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Award",[["path",{d:"m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526",key:"1yiouv"}],["circle",{cx:"12",cy:"8",r:"6",key:"1vp47v"}]])},7592:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]])},3231:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]])},933:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Clock",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]])},7668:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Gem",[["path",{d:"M6 3h12l4 6-10 13L2 9Z",key:"1pcd5k"}],["path",{d:"M11 3 8 9l4 13 4-13-3-6",key:"1fcu3u"}],["path",{d:"M2 9h20",key:"16fsjt"}]])},2271:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Laptop",[["path",{d:"M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16",key:"tarvll"}]])},5293:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Leaf",[["path",{d:"M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z",key:"nnexq3"}],["path",{d:"M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12",key:"mt58a7"}]])},6706:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("RefreshCw",[["path",{d:"M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8",key:"v9h5vc"}],["path",{d:"M21 3v5h-5",key:"1q7to0"}],["path",{d:"M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16",key:"3uifl3"}],["path",{d:"M8 16H3v5",key:"1cv678"}]])},500:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Shield",[["path",{d:"M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z",key:"oel41y"}]])},8400:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Smartphone",[["rect",{width:"14",height:"20",x:"5",y:"2",rx:"2",ry:"2",key:"1yt0o3"}],["path",{d:"M12 18h.01",key:"mhygvu"}]])},9338:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Star",[["polygon",{points:"12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2",key:"8f66p6"}]])},7359:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Tablet",[["rect",{width:"16",height:"20",x:"4",y:"2",rx:"2",ry:"2",key:"76otgf"}],["line",{x1:"12",x2:"12.01",y1:"18",y2:"18",key:"1dp563"}]])},1240:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]])},3045:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Wrench",[["path",{d:"M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z",key:"cbrjhi"}]])},5430:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(8030).Z)("Zap",[["path",{d:"M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z",key:"1xq2db"}]])},6648:function(e,t,n){n.d(t,{default:function(){return o.a}});var r=n(5601),o=n.n(r)},7138:function(e,t,n){n.d(t,{default:function(){return o.a}});var r=n(231),o=n.n(r)},5601:function(e,t,n){Object.defineProperty(t,"__esModule",{value:!0}),function(e,t){for(var n in t)Object.defineProperty(e,n,{enumerable:!0,get:t[n]})}(t,{default:function(){return l},getImageProps:function(){return c}});let r=n(9920),o=n(497),i=n(8173),u=r._(n(1241));function c(e){let{props:t}=(0,o.getImgProps)(e,{defaultLoader:u.default,imgConf:{deviceSizes:[640,750,828,1080,1200,1920,2048,3840],imageSizes:[16,32,48,64,96,128,256,384],path:"/_next/image/",loader:"default",dangerouslyAllowSVG:!1,unoptimized:!0}});for(let[e,n]of Object.entries(t))void 0===n&&delete t[e];return{props:t}}let l=i.Image},4446:function(e,t,n){n.d(t,{M:function(){return k}});var r=n(7437),o=n(2265),i=n(5050),u=n(458),c=n(7797),l=n(9791);class a extends o.Component{getSnapshotBeforeUpdate(e){let t=this.props.childRef.current;if(t&&e.isPresent&&!this.props.isPresent){let e=this.props.sizeRef.current;e.height=t.offsetHeight||0,e.width=t.offsetWidth||0,e.top=t.offsetTop,e.left=t.offsetLeft}return null}componentDidUpdate(){}render(){return this.props.children}}function f(e){let{children:t,isPresent:n}=e,i=(0,o.useId)(),u=(0,o.useRef)(null),c=(0,o.useRef)({width:0,height:0,top:0,left:0}),{nonce:f}=(0,o.useContext)(l._);return(0,o.useInsertionEffect)(()=>{let{width:e,height:t,top:r,left:o}=c.current;if(n||!u.current||!e||!t)return;u.current.dataset.motionPopId=i;let l=document.createElement("style");return f&&(l.nonce=f),document.head.appendChild(l),l.sheet&&l.sheet.insertRule('\n          [data-motion-pop-id="'.concat(i,'"] {\n            position: absolute !important;\n            width: ').concat(e,"px !important;\n            height: ").concat(t,"px !important;\n            top: ").concat(r,"px !important;\n            left: ").concat(o,"px !important;\n          }\n        ")),()=>{document.head.removeChild(l)}},[n]),(0,r.jsx)(a,{isPresent:n,childRef:u,sizeRef:c,children:o.cloneElement(t,{ref:u})})}let d=e=>{let{children:t,initial:n,isPresent:i,onExitComplete:l,custom:a,presenceAffectsLayout:d,mode:h}=e,p=(0,u.h)(s),y=(0,o.useId)(),m=(0,o.useCallback)(e=>{for(let t of(p.set(e,!0),p.values()))if(!t)return;l&&l()},[p,l]),k=(0,o.useMemo)(()=>({id:y,initial:n,isPresent:i,custom:a,onExitComplete:m,register:e=>(p.set(e,!1),()=>p.delete(e))}),d?[Math.random(),m]:[i,m]);return(0,o.useMemo)(()=>{p.forEach((e,t)=>p.set(t,!1))},[i]),o.useEffect(()=>{i||p.size||!l||l()},[i]),"popLayout"===h&&(t=(0,r.jsx)(f,{isPresent:i,children:t})),(0,r.jsx)(c.O.Provider,{value:k,children:t})};function s(){return new Map}var h=n(3241);let p=e=>e.key||"";function y(e){let t=[];return o.Children.forEach(e,e=>{(0,o.isValidElement)(e)&&t.push(e)}),t}var m=n(9033);let k=e=>{let{children:t,custom:n,initial:c=!0,onExitComplete:l,presenceAffectsLayout:a=!0,mode:f="sync",propagate:s=!1}=e,[k,g]=(0,h.oO)(s),v=(0,o.useMemo)(()=>y(t),[t]),Z=s&&!k?[]:v.map(p),x=(0,o.useRef)(!0),M=(0,o.useRef)(v),C=(0,u.h)(()=>new Map),[w,b]=(0,o.useState)(v),[E,R]=(0,o.useState)(v);(0,m.L)(()=>{x.current=!1,M.current=v;for(let e=0;e<E.length;e++){let t=p(E[e]);Z.includes(t)?C.delete(t):!0!==C.get(t)&&C.set(t,!1)}},[E,Z.length,Z.join("-")]);let j=[];if(v!==w){let e=[...v];for(let t=0;t<E.length;t++){let n=E[t],r=p(n);Z.includes(r)||(e.splice(t,0,n),j.push(n))}"wait"===f&&j.length&&(e=j),R(y(e)),b(v);return}let{forceRender:z}=(0,o.useContext)(i.p);return(0,r.jsx)(r.Fragment,{children:E.map(e=>{let t=p(e),o=(!s||!!k)&&(v===E||Z.includes(t));return(0,r.jsx)(d,{isPresent:o,initial:(!x.current||!!c)&&void 0,custom:o?void 0:n,presenceAffectsLayout:a,mode:f,onExitComplete:o?void 0:()=>{if(!C.has(t))return;C.set(t,!0);let e=!0;C.forEach(t=>{t||(e=!1)}),e&&(null==z||z(),R(M.current),s&&(null==g||g()),l&&l())},children:e},t)})})}},7476:function(e,t,n){n.d(t,{Y:function(){return u}});var r=n(2265),o=n(6167);let i={some:0,all:1};function u(e,{root:t,margin:n,amount:u,once:c=!1}={}){let[l,a]=(0,r.useState)(!1);return(0,r.useEffect)(()=>{if(!e.current||c&&l)return;let r={root:t&&t.current||void 0,margin:n,amount:u};return function(e,t,{root:n,margin:r,amount:u="some"}={}){let c=(0,o.IG)(e),l=new WeakMap,a=new IntersectionObserver(e=>{e.forEach(e=>{let n=l.get(e.target);if(!!n!==e.isIntersecting){if(e.isIntersecting){let n=t(e);"function"==typeof n?l.set(e.target,n):a.unobserve(e.target)}else"function"==typeof n&&(n(e),l.delete(e.target))}})},{root:n,rootMargin:r,threshold:"number"==typeof u?u:i[u]});return c.forEach(e=>a.observe(e)),()=>a.disconnect()}(e.current,()=>(a(!0),c?void 0:()=>a(!1)),r)},[t,e,n,c,u]),l}}}]);
---
+++ dist/images/repairs/README.md
@@ -0,0 +1,32 @@
+# Repair Service Images
+
+This directory should contain specific images for each repair service.
+
+## Required Images
+
+Add transparent PNG images with white backgrounds for each repair type:
+
+- `screen.png` - Screen repair service image
+- `battery.png` - Battery replacement service image
+- `camera.png` - Camera repair service image
+- `charging-port.png` - Charging port repair service image
+- `water-damage.png` - Water damage repair service image
+- `diagnostics.png` - Diagnostic service image
+- `speaker.png` - Speaker repair service image
+- `software.png` - Software repair service image
+- `back-glass.png` - Back glass repair service image
+- `earpiece.png` - Earpiece repair service image
+- `home-button.png` - Home button repair service image
+- `back-cover.png` - Back cover repair service image
+
+## Image Specifications
+
+- Format: PNG with transparent background
+- Size: Recommended 400x400px or larger
+- Background: Transparent (will display on white background)
+- Style: Royalty-free, professional device repair images
+
+## Fallback
+
+If a repair-specific image is not found, the system will fall back to `/images/mobile.png`.
+
